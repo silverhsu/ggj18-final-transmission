524fd8090ea8309e26cd6d6cd26bc97fff8fe440
Jeffrey Hsu <silverhsu@gmail.com>
2018-01-28 12:54:10 -0600

    Merged

---
+++ GGJ-Final-Transmission/Assets/Scripts/DestroyedShip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyedShip : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    
+	void Start ()
+    {
+        rb.rotation = Random.Range(0.0f, 360.0f);
+        rb.angularVelocity = Random.Range(1, 5) * 30.0f;
+        rb.velocity = new Vector2(0.0f, -1.0f);
+
+    }
+}
