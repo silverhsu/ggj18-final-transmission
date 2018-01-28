27104e548952fc1efbdb61eb2a04e4ecc98991ea
Jeffrey Hsu <silverhsu@gmail.com>
2018-01-28 13:35:23 -0600

    Added edited planet sprites, made Main Menu and Credits Screen.

---
+++ GGJ-Final-Transmission/Assets/EffectMirrorTextScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectMirrorTextScript : MonoBehaviour {
+
+    private TextMesh parentTxtMesh;
+    private TextMesh txtMesh;
+
+	// Use this for initialization
+	void Start () {
+        txtMesh = this.GetComponent<TextMesh>();
+        parentTxtMesh = this.transform.parent.GetComponent<TextMesh>();
+        updateSettings();
+	}
+
+
+    // Update is called once per frame
+
+    void Update () {
+		if(txtMesh.text.GetHashCode() != parentTxtMesh.text.GetHashCode())
+        {
+            updateSettings();
+        }
+	}
+
+    void updateSettings()
+    {
+        txtMesh.text = parentTxtMesh.text;
+        txtMesh.alignment = parentTxtMesh.alignment;
+        txtMesh.fontSize = parentTxtMesh.fontSize;
+        txtMesh.anchor = parentTxtMesh.anchor;
+        txtMesh.characterSize = parentTxtMesh.characterSize;
+    }
+}
---
+++ GGJ-Final-Transmission/Assets/UIMainMenuButtonScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum MainMenuButtonEnum { START , CREDITS, BACK};
+
+public class UIMainMenuButtonScript : MonoBehaviour {
+
+    public MainMenuButtonEnum thisButtonType;
+    private Collider2D col;
+
+    private Vector3 startScale;
+
+
+	// Use this for initialization
+	void Start () {
+        col = this.GetComponent<Collider2D>();
+        startScale = this.transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localScale = Vector3.Lerp(this.transform.localScale, startScale, Time.deltaTime * 8f);
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            switch (thisButtonType)
+            {
+                case MainMenuButtonEnum.START:
+                    SceneManager.LoadScene("TestScene");
+                    break;
+                case MainMenuButtonEnum.CREDITS:
+                    SceneManager.LoadScene("Credits");
+                    break;
+                case MainMenuButtonEnum.BACK:
+                    SceneManager.LoadScene("MainMenu");
+                    break;
+                default:
+                    SceneManager.LoadScene("MainMenu");
+                    break;
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            this.transform.localScale = Vector3.Lerp(this.transform.localScale, startScale * 1.5f, Time.deltaTime * 8f);
+        }
+    }
+
+}
